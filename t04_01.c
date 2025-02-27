5013fd12e46019b2d9c2c7d05e113d8152affb65
Grasiasimanullang <grasiagayatrisimanullang@gmail.com>
2025-02-27 14:31:59 +0700

    pair 02

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,22 @@
-// NIM - Name
-// NIM - Name
+// 12S24008 - Rospika Sarah Yosefin Siregar
+// 12S24027 - Grasia Gayatri Simanullang
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+  char input[61];
+  scanf("%s", input);
+    
+    int lenght = strlen(input);
+    for (int i = 0; i < lenght; i+=3) {
+        char a[4] = {input[i], input[i+1], input[i+2], '\0'}; 
+        int ascii = atoi(a); 
+        printf("%c", ascii);  
+    }
+    
+    printf("\n");  
 
   return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,25 @@
-// NIM - Name
-// NIM - Name
+// 12S24008 - Rospika Sarah Yosefin Siregar
+// 12S24027 - Grasia Gayatri Simanullang
+
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
-
-  return 0;
+  char karakter[21];  
+    fgets(karakter, sizeof(karakter), stdin);
+    
+    size_t lenght = strlen(karakter);
+    if (lenght > 0 && karakter[lenght - 1] == '\n') {
+        karakter[lenght - 1] = '\0';
+        lenght--;
+    }
+    
+    for (size_t i = 0; i < lenght; i++) {
+        printf("%03d", karakter[i]);
+    }
+    
+    printf("\n");
+    return 0;
 }
+
